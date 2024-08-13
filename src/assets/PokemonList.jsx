bb27fb11458cdd5a5116a74fd497de1eb736d369
Msambere <110812972+Msambere@users.noreply.github.com>
2024-08-13 18:37:40 -0400

    Add more pokemon to the list

--- src/assets/PokemonList.jsx
+++ src/assets/PokemonList.jsx
@@ -1,16 +1,25 @@
 import { v4 as uuid4 } from "uuid";
 
 const pokemonList = [
-  { id: uuid4(), name: "pikachu", clicked: false },
-  { id: uuid4(), name: "charizard", clicked: false },
-  { id: uuid4(), name: "eevee", clicked: false },
-  { id: uuid4(), name: "bulbasaur", clicked: false },
-  { id: uuid4(), name: "umbreon", clicked: false },
-  { id: uuid4(), name: "gengar", clicked: false },
-  { id: uuid4(), name: "squirtle", clicked: false },
-  { id: uuid4(), name: "articuno", clicked: false },
-  { id: uuid4(), name: "snorlax", clicked: false },
-  { id: uuid4(), name: "mew", clicked: false },
+  { id: uuid4(), name: "pikachu" },
+  { id: uuid4(), name: "charmander" },
+  { id: uuid4(), name: "eevee" },
+  { id: uuid4(), name: "bulbasaur" },
+  { id: uuid4(), name: "umbreon" },
+  { id: uuid4(), name: "gengar" },
+  { id: uuid4(), name: "squirtle" },
+  { id: uuid4(), name: "articuno" },
+  { id: uuid4(), name: "snorlax" },
+  { id: uuid4(), name: "mew" },
+  { id: uuid4(), name: "rattata" },
+  { id: uuid4(), name: "diglett" },
+  { id: uuid4(), name: "meowth" },
+  { id: uuid4(), name: "psyduck" },
+  { id: uuid4(), name: "bellsprout" },
+  { id: uuid4(), name: "slowpoke" },
+  { id: uuid4(), name: "onix" },
+  { id: uuid4(), name: "goldeen" },
+  { id: uuid4(), name: "ditto" },
 ];
 
 export { pokemonList };
